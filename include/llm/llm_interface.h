1f2b2366fd8837e30651decb1efacde8effa9036
NGOIE BAKAFWA ELVIC <elvicbeone@gmail.com>
2026-04-23 15:45:19 -0800

    Ajout de llfeat(llm): add llama.cpp et creation de fichier llm_interface.h et hunspell_wrap.h

---
+++ include/llm/llm_interface.h
@@ -0,0 +1,4 @@
+#ifndef LLM_LLM_INTERFACE_H
+#define LLM_LLM_INTERFACE_H
+int llm_init_dummy(const char *model_path);
+#endif
---
+++ include/nlp/hunspell_wrap.h
@@ -0,0 +1,4 @@
+#ifndef NLP_HUNSPELL_WRAP_H
+#define NLP_HUNSPELL_WRAP_H
+int spell_check_word_dummy(const char *word);
+#endif
